8e31a34fb6137e54ac68318347df28c818fbfe46
nikx12 <nikhitazulka@gmail.com>
2018-11-11 18:25:24 +0530

    Modified logic for send button

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,18 @@
 $(function(){
     //make a connection
     
-	var socket = io.connect('http://localhost:3001')
+    var socket = io.connect('http://localhost:3001');
+    //buttons and inputs
+	var message = $("#message")
+	var username = $("#username")
+	var send_message = $("#send_message")
+	var send_username = $("#send_username")
+	var chatroom = $("#chatroom")
+	var feedback = $("#feedback")
+
+	//Emit message
+	send_message.click(function(){
+        socket.emit('new_message', {message : message.val()})
+        console.log("send message clicked", message.val())
+    })
 })
